d88b7450695f05e5f69148c8c13a9e174414ff44
tglynn2 <tglynn2@oswego.edu>
2026-02-07 21:06:04 +0000

    Can now create everything(very hackily)

--- demo/src/main/java/com/example/demo/controllers/UserController.java
+++ demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -5,15 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.Day;
+import com.example.demo.models.GymSet;
 import com.example.demo.models.User;
 import com.example.demo.models.Workout;
 import com.example.demo.services.DayService;
 import com.example.demo.services.UserService;
+import com.example.demo.services.WorkoutService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -29,6 +32,9 @@ public class UserController {
     @Autowired
     DayService dayService;
 
+    @Autowired
+    WorkoutService workoutService;
+
     @PostMapping("/create")
     public User createUser(@RequestParam(required = true) String username){
         User user = new User();
@@ -37,7 +43,8 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public User login(@RequestParam(required = true) String username, HttpServletRequest request){
+    public User login(@RequestParam(required = true) String username,
+        HttpServletRequest request){
         User user = userService.getUserByUsername(username).orElse(null);
         if(user != null){
             request.getSession().setAttribute("id", user.getId());
@@ -46,7 +53,8 @@ public class UserController {
     }
     
     @PostMapping("/create/day")
-    public ResponseEntity<?> createDay(HttpServletRequest request, @RequestParam(required = true) String name){
+    public ResponseEntity<?> createDay(HttpServletRequest request,
+        @RequestParam(required = true) String name){
         HttpSession session = request.getSession(false);
         if(session == null || session.getAttribute("id") == null){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login First");
@@ -64,7 +72,9 @@ public class UserController {
     }
 
     @PostMapping("/create/workout")
-    public ResponseEntity<?> createWorkout(HttpServletRequest request, @RequestParam(required = true) String dayName, @RequestParam(required = true) String workoutName){
+    public ResponseEntity<?> createWorkout(HttpServletRequest request,
+        @RequestParam(required = true) String dayName,
+        @RequestParam(required = true) String workoutName){
         HttpSession session = request.getSession(false);
         if(session == null || session.getAttribute("id") == null){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login first");
@@ -80,4 +90,25 @@ public class UserController {
         userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
+
+    @RequestMapping("/create/set")
+    public ResponseEntity<?> createSet(HttpServletRequest request, 
+        @RequestBody GymSet gymSet, 
+        @RequestParam(required = true) String dayName, 
+        @RequestParam(required = true) String workoutName){
+            HttpSession session = request.getSession(false);
+            if(session == null || session.getAttribute("id") == null){
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login first");
+            }
+            UUID id = UUID.fromString(session.getAttribute("id").toString());
+            User user = userService.findById(id).orElse(null);
+            Day day = dayService.getDayByName(dayName).orElse(null);
+            Workout workout = workoutService.findByName(workoutName).orElse(null);
+            gymSet.setWorkout(workout);
+            workout.getSets().add(gymSet);
+            day.getWorkouts().add(workout);
+            user.getDays().add(day);
+            userService.saveUser(user);
+            return ResponseEntity.status(HttpStatus.OK).body(user);    
+        }
 }
